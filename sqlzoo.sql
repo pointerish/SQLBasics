cad6a4f25adf2ef4f434773dd7470b75f0fa8984
Josias Alvarado <josiasjag@gmail.com>
2020-12-02 15:30:43 -0600

    Solve all SELECT FROM world exercises.

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -115,3 +115,71 @@ SELECT name FROM world WHERE population >= 200000000;
 
 SELECT name, (gdp / population) AS 'per capita GDP'
 FROM world WHERE population >= 200000000;
+
+--4
+
+SELECT name, (population / 1000000) as 'population'
+FROM world
+WHERE continent = 'South America';
+
+--5
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+--6
+
+SELECT name
+FROM world
+WHERE INSTR(name, 'United');
+
+--7
+
+SELECT name, population, area
+FROM world
+WHERE area >= 3000000
+OR population >= 250000000;
+
+--8
+
+SELECT name, population, area
+FROM world
+WHERE area >= 3000000
+XOR population >= 250000000;
+
+--9
+
+SELECT name, ROUND((population / 1000000), 2) AS 'population', ROUND((gdp / 1000000000), 2) AS 'gdp'
+FROM world
+WHERE continent = 'South America';
+
+--10
+
+SELECT name, ROUND((gdp/population), -3) AS 'per-capita GDP'
+FROM world
+WHERE gdp >= 1000000000000;
+
+--11
+
+SELECT name, capital
+FROM world
+WHERE LENGTH(capital) = LENGTH(name);
+
+--12
+
+SELECT name, capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1)
+AND name <> capital;
+
+--13
+
+SELECT name
+FROM world
+WHERE INSTR(name, 'a') > 0
+AND INSTR(name, 'e') > 0
+AND INSTR(name, 'i') > 0
+AND INSTR(name, 'o') > 0
+AND INSTR(name, 'u') > 0
+AND name NOT LIKE '% %';
