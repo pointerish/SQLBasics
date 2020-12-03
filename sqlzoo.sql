b0451a3bbe687d37955a078c9dfddca607744bb4
Josias Alvarado <josiasjag@gmail.com>
2020-12-03 11:50:44 -0600

    Solve 7 exercises for nested SELECT section.

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -281,3 +281,62 @@ SELECT winner, subject
  ORDER BY
  CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END,
   subject,winner;
+
+-- SELECT within SELECT
+
+--1
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+--2
+
+SELECT name FROM world
+  WHERE (gdp/population) >
+     (SELECT (gdp/population) FROM world
+      WHERE name='United Kingdom')
+  AND continent = 'Europe';
+
+--3
+
+SELECT name, continent FROM world
+  WHERE continent IN 
+    (SELECT continent FROM world
+       WHERE name IN ('Argentina', 'Australia'))
+  ORDER BY name;
+
+--4
+
+SELECT name, population
+FROM world
+WHERE population >
+  (SELECT population FROM world WHERE name = 'Canada')
+AND population <
+  (SELECT population FROM world WHERE name = 'Poland');
+
+--5
+
+SELECT name, CONCAT(ROUND(100* (population/(SELECT population
+                           FROM world
+                           WHERE name='Germany'))), '%')
+FROM world
+WHERE continent='Europe';
+
+--6
+
+SELECT name FROM world
+WHERE gdp > ALL(SELECT gdp
+                FROM world
+                WHERE continent='Europe'
+                AND gdp IS NOT NULL)
+AND continent != 'Europe';
+
+--7
+
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
