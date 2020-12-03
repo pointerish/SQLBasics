59a25643d6840287ba55c6abb0438af65049c80d
Josias Alvarado <josiasjag@gmail.com>
2020-12-03 16:08:42 -0600

    Add more solved exercises.

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -410,3 +410,44 @@ GROUP BY continent;
 SELECT continent FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
+
+--JOIN operation
+
+--1
+
+SELECT matchid, player FROM goal 
+  WHERE teamid = 'GER';
+
+--2
+
+SELECT id,stadium,team1,team2
+  FROM game
+  WHERE id = 1012;
+
+--3
+
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+  WHERE teamid='GER';
+
+--4
+
+SELECT team1,team2,player
+  FROM game JOIN goal ON (id=matchid)
+  WHERE player LIKE 'Mario%';
+
+--5
+
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam on teamid=id 
+ WHERE gtime<=10
+
+--6
+
+SELECT mdate, teamname FROM game
+  JOIN eteam ON (team1=eteam.id AND coach = 'Fernando Santos')
+
+
+
+
+
