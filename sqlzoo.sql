200f87f7552e1fc535eafe0201c68ab1466bfcaa
Josias Alvarado <josiasjag@gmail.com>
2020-12-03 16:36:57 -0600

    Complete JOIN exercises.

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -447,7 +447,46 @@ SELECT player, teamid, coach, gtime
 SELECT mdate, teamname FROM game
   JOIN eteam ON (team1=eteam.id AND coach = 'Fernando Santos')
 
+--7
 
+SELECT player FROM goal JOIN game ON (matchid=id)
+WHERE stadium='National Stadium, Warsaw';
 
+--8
 
+SELECT DISTINCT(player) FROM game
+  JOIN goal ON (matchid=id)
+WHERE ((team1='GER' OR team2='GER') AND teamid != 'GER')
 
+--9
+
+SELECT teamname, COUNT(player) FROM eteam
+  JOIN goal ON (id=teamid)
+GROUP BY teamname
+
+--10
+
+SELECT DISTINCT(stadium), COUNT(goal.player)
+FROM game JOIN goal ON (matchid=id)
+GROUP BY stadium;
+
+--11
+
+SELECT matchid, mdate, COUNT(player) FROM game
+JOIN goal ON (matchid=id AND (team1 = 'POL' OR team2 = 'POL'))
+GROUP BY matchid, mdate;
+
+--12
+
+SELECT id, mdate, COUNT(player) FROM game
+  JOIN goal ON (id=matchid AND (team1 = 'GER' OR team2 = 'GER') AND teamid='GER')
+GROUP BY id, mdate;
+
+--13
+
+SELECT mdate, team1, SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+       team2,
+       SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 FROM
+    game LEFT JOIN goal ON (id=matchid)
+    GROUP BY mdate, team1, team2
+    ORDER BY mdate, matchid, team1, team2
