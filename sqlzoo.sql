8511eb972102ae79408656b585161479d7cfa150
Josias Alvarado <josiasjag@gmail.com>
2020-12-04 11:32:17 -0600

    Complete more exercises.

--- sqlzoo.sql
+++ sqlzoo.sql
@@ -595,3 +595,97 @@ WHERE movieid IN (
   SELECT movieid FROM casting JOIN actor ON (actorid=id AND name='Art Garfunkel')) 
   AND name != 'Art Garfunkel'
 GROUP BY name;
+
+--USING NULL
+
+--1
+
+SELECT name
+FROM teacher
+WHERE dept IS NULL;
+
+--2
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id);
+
+--3
+
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+ON (teacher.dept=dept.id);
+
+--4
+
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept
+ON (teacher.dept=dept.id);
+
+--5
+
+SELECT name,
+COALESCE(mobile, '07986 444 2266')
+FROM teacher;
+
+--6
+
+SELECT COALESCE(teacher.name, 'NONE'), COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
+
+--7
+
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher;
+
+--8
+
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name
+
+--9
+
+SELECT teacher.name,
+CASE WHEN dept.id = 1 THEN 'Sci'
+     WHEN dept.id = 2 THEN 'Sci'
+     ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+--10
+
+SELECT teacher.name,
+CASE
+WHEN dept.id = 1 THEN 'Sci'
+WHEN dept.id = 2 THEN 'Sci'
+WHEN dept.id = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (dept.id=teacher.dept)
+
+--Window Function
+
+--1
+
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC;
+
+--2
+
+SELECT party, votes,
+       RANK() OVER (ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party;
+
+--3
+
+SELECT yr,party, votes,
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000021'
+ORDER BY party,yr
+
+--4
+
